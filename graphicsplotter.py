98f77999bbe43d22de9e64ea3abafc3975e28b1a
Merlin Böschen <merboe@uni-bremen.de>
2021-08-11 17:05:16 +0200

    Working on Einlesen Module in solvers.py

--- graphicsplotter.py
+++ graphicsplotter.py
@@ -5,38 +5,39 @@ import numpy as np
 
 projectname = "firsttest"
 
+
 def read_in (projectname):
-    
+
     #unpacking energies
     energies = np.loadtxt(os.path.join("output/data_submitted", projectname, "energies.dat"))
     #unpacking Potential and x_values
     discrpot = np.loadtxt(os.path.join("output/data_submitted", projectname, "discrpot.dat"))
-    
+
     x_values = discrpot[:,0]
     discropt_values =  discrpot[:,1]
-    
-    #unpacking wfuncs    
+
+    #unpacking wfuncs
     wfuncs = np.loadtxt(os.path.join("output/data_submitted", projectname, "wfuncs.dat"))
-    
+
     wfuncs_values = wfuncs[:,1:]
-     
-            
+
+
    return energies, x_values, discropt_values, wfuncs_values
 
 
 
 
 def plotting(energies, x_values, discropt_values, wfuncs_values, factor=1, factor1=1, x_bound=None , y_max=None ):
-    
-    
+
+
     plt.figure(figsize=(15, 7.5), dpi=80)
     plt.subplot(1,2,1)
-  
+
     #plotting Potential:
     plt.plot(x_values, discropt_values)
     #plotting Wfuncs:
     plt.plot(x_values, wfuncs_values*factor + energies)
-    #x_range:    
+    #x_range:
     if x_bound is None:
         plt.xlim(x_values.min()*1.1,x_values.max()*1.1)
     else:
@@ -46,9 +47,9 @@ def plotting(energies, x_values, discropt_values, wfuncs_values, factor=1, facto
         plt.ylim((wfuncs_values*factor +energies).min()-0.5, (wfuncs_values*factor +energies).max()*1.1)
     else:
         plt.ylim((wfuncs_values*factor +energies).min()-0.5, y_max)
-    
+
     plt.subplot(1,2,2)
-    
+
     #plotting Potential:
     plt.plot(x_values, discropt_values)
     #plotting Wfuncs:
@@ -63,5 +64,5 @@ def plotting(energies, x_values, discropt_values, wfuncs_values, factor=1, facto
         plt.ylim((np.abs(wfuncs_values)**2 *factor1 + energies).min()-0.5, (np.abs(wfuncs_values)**2 *factor1 + energies).max()*1.1)
     else:
         plt.ylim((np.abs(wfuncs_values)**2 *factor1 + energies).min()-0.5, y_max)
-    
+
     plt.show()
--- solver.py
+++ solver.py
@@ -1,2 +1,49 @@
 # -*- coding: utf-8 -*-
+import os.path
+import numpy as np
+from scipy import scipy.linalg.eigh_tridiagonal()
 
+
+def Einlesen(input_directory):
+    """Soll die Inputdatei (schrodinger.inp) auslesen
+    und die darin enthaltenen Daten erkennen
+    und als jeweiligen Wert ausgeben
+
+    Args:
+        input_directory: the directory path where to find schrodinger.int
+
+    Returns:
+        the data needed for Solver() function
+        or Error message if Input dat. not found or couldnt be read
+
+
+    """
+    directory = "input_directory"
+    input = np.loadtxt(os.path.join(directory, schrodinger.dat))
+    mass = input[0]  # Masse
+    xMin = input[1, 0]  # minimale x Koordinate
+    xMax = input[1, 1]  # maximale x Koordinate
+    nPoint = input[1, 2]  # anzahl an punkten dazwischen
+    ev1 = input[2, 0]  # first eigenvalue to print
+    ev2 = input[2, 1]  # last eigenvalue to print
+    interpol = input[3],  # hier noch ein default = linear irgendwie einbauen
+    if interpol is not linear, polynomial, cspline:
+        print("Wrong Interpolation type")
+        break
+    points = input[4]  # anzahl der interpolationspunkte
+    xPot = input[5:, 0]  # x koordinate des Potentials
+    yPot = input[5:, 1]  # y koordinate des Potentials
+
+    return mass, xMin, xMax, nPoin, ev1, ev2, interpol, points, xPot, yPot
+
+
+def Solver():
+    """Soll die vorhin ausgegebenen Daten in die benötigten Werte umrechnen
+
+    """
+    potential.dat
+    energies.dat
+    wavefuncs.dat
+    expvalues.dat
+
+    return lösung
